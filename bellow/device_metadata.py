ad9f0919d925200444d1ab97d09eb02adc3cf17d
Daniel J. Parente <dan.parente@gmail.com>
2023-09-21 00:13:14 -0500

    Refactor to move functions that get metadata for audio devices to their own module

---
+++ bellow/device_metadata.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+import sounddevice as sd
+
+
+def list_devices() -> None:
+    """
+    Prints a list of all input and output audio devices
+
+    :return: None
+    """
+    devices = sd.query_devices()
+    print(devices)
+
+
+def get_device_name(idx: Optional[int] = None, input_if_default: Optional[bool] = None) -> str:
+    """
+    Returns the name of a device with index idx. If this is none, it returns the name of the
+    default input or output device depending on the value of input_if_default. Both inputs cannot
+    be simultaneously unspecified (None).
+
+    :param idx: The index of the device in the query_devices list
+    :param input_if_default: If idx is None, should we return the default input device (if false, return output device)
+    :return: The name of the specified device
+    """
+    # If index is none, get one of the default devices
+    if idx is None:
+        if input_if_default is None:
+            raise ValueError('Cannot retrieve input/output device if input_if_default is None')
+
+        if input_if_default:
+            return sd.query_devices(sd.default.device[0])['name']
+        else:
+            return sd.query_devices(sd.default.device[1])['name']
+
+    # Otherwise return name by index
+    devices = sd.query_devices()
+    return devices[idx]['name']
--- bellow/main.py
+++ bellow/main.py
@@ -6,9 +6,10 @@ from threading import Event, Semaphore
 import sounddevice as sd
 import keyboard
 import clipboard
+
+from bellow.device_metadata import list_devices, get_device_name
 from bellow.soundeffects import play_effect
 from transformers import pipeline
-from typing import Optional
 import logging
 
 # Set up logging
@@ -172,41 +173,6 @@ def handle_dump() -> None:
 
     if not halt_recording.is_set():
         set_halt(dump_audio=True)
-
-
-def list_devices() -> None:
-    """
-    Prints a list of all input and output audio devices
-
-    :return: None
-    """
-    devices = sd.query_devices()
-    print(devices)
-
-
-def get_device_name(idx: Optional[int] = None, input_if_default: Optional[bool] = None) -> str:
-    """
-    Returns the name of a device with index idx. If this is none, it returns the name of the
-    default input or output device depending on the value of input_if_default. Both inputs cannot
-    be simultaneously unspecified (None).
-
-    :param idx: The index of the device in the query_devices list
-    :param input_if_default: If idx is None, should we return the default input device (if false, return output device)
-    :return: The name of the specified device
-    """
-    # If index is none, get one of the default devices
-    if idx is None:
-        if input_if_default is None:
-            raise ValueError('Cannot retrieve input/output device if input_if_default is None')
-
-        if input_if_default:
-            return sd.query_devices(sd.default.device[0])['name']
-        else:
-            return sd.query_devices(sd.default.device[1])['name']
-
-    # Otherwise return name by index
-    devices = sd.query_devices()
-    return devices[idx]['name']
 
 
 def main() -> None:
